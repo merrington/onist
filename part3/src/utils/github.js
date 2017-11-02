2216b581232af8a705d67ee55925fff5ebce1bd8
Mike Errington <mike.errington@gmail.com>
2017-11-01 23:58:26 -0400

    Fix github results

--- part3/src/utils/github.js
+++ part3/src/utils/github.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const ghApi = require('github');
 
 const gh = new ghApi();
@@ -17,6 +18,29 @@ class Github {
       });
   }
 
+  async handlePagedResults(cb) {
+    let allResults = [];
+    let moreResults = true;
+    let page = 1;
+
+    while (moreResults) {
+      this.addToken();
+      const response = await cb(page);
+      const results = _.get(response, 'data', []);
+
+      if (results) {
+        allResults = allResults.concat(results);
+        page += 1;
+        if (results.length < pageSize) {
+          moreResults = false;
+        }
+      } else {
+        moreResults = false;
+      }
+    }
+    return allResults;
+  }
+
   async getOrg(org) {
     try {
       this.addToken();
@@ -30,34 +54,51 @@ class Github {
 
   async getMembers(org) {
     try {
-      let allMembers = [];
-      let moreMembers = true;
-      let page = 1;
-
-      while (moreMembers) {
-        this.addToken();
-        const result = await this.gh.orgs.getMembers({
+      const members = await this.handlePagedResults(page => {
+        return this.gh.orgs.getMembers({
           org,
           per_page: pageSize,
           page
         });
-        const members = result && result.data;
+      });
 
-        if (members && Array.isArray(members)) {
-          allMembers = allMembers.concat(members);
-          page += 1;
-          if (members.length < pageSize) {
-            moreMembers = false;
-          }
-        } else {
-          moreMembers = false;
-        }
-      }
-
-      return allMembers;
+      return (await Promise.all(
+        members.map(async member => ({
+          login: member.login,
+          avatar_url: member.avatar_url,
+          followers: await this.getFollowers(member.login),
+          following: await this.getFollowing(member.login)
+        }))
+      )).sort((m1, m2) => m2.followers - m1.followers);
     } catch (e) {
       console.error(e);
       return [];
+    }
+  }
+
+  async getFollowers(username) {
+    try {
+      const followers = await this.handlePagedResults(page => {
+        return this.gh.users.getFollowersForUser({ username });
+      });
+
+      return followers.length;
+    } catch (e) {
+      console.error(e);
+      return 0;
+    }
+  }
+
+  async getFollowing(username) {
+    try {
+      const following = await this.handlePagedResults(page => {
+        return this.gh.users.getFollowingForUser({ username });
+      });
+
+      return following.length;
+    } catch (e) {
+      console.error(e);
+      return 0;
     }
   }
 }
